71667b4ad6cea0867ecf425a605994c34e013c15
yisheng zhang <135839916+xyz520wild@users.noreply.github.com>
2024-01-14 00:54:17 +0800

    更新 glados.py
    更新青龙多账号

--- glados.py
+++ glados.py
@@ -1,9 +1,27 @@
+import os
 import requests
-
+​
+# 获取JD_COOKIE环境变量
+cookies = os.environ.get('gladosCookie')
+​
+# 将多个Cookie分割成列表
+cookies_list = cookies.split('n/')
+# 输出有几个Cookie
+print(f"总共有 {len(cookies_list)} 个Cookie。")
+​
+​
+# 目标URL
 url = "https://glados.rocks/api/user/checkin"
-gladosCookie = "koa:sess=eyJ1c2VySWQiOjQyNTgzMCwiX2V4cGlyZSI6MTczMTA1MjQ1MTM4MywiX21heEFnZSI6MjU5MjAwMDAwMDB9; koa:sess.sig=_rFG_6WWBjrMWUDragkkYKDan2M"
-
-headers = {
+# 要发送的数据
+data = '{"token":"glados.one"}'
+​
+# 初始化响应列表
+responses = []
+​
+# 使用索引遍历cookies_list
+for i in range(len(cookies_list)):
+    current_cookie = cookies_list[i]
+    headers =  {
     "Host": "glados.rocks",
     "Connection": "keep-alive",
     "Content-Length": "22",
@@ -20,17 +38,23 @@ headers = {
     "Sec-Fetch-Dest": "empty",
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-    "Cookie": gladosCookie }
-
-data = '{"token":"glados.one"}'
-
+    "Cookie": current_cookie }
+    
+​
+ 
+​
+# 在此处处理 responses 列表，可以根据需求进一步操作每个账号的响应
+​
+​
 response = requests.post(url, headers=headers, data=data)
-
+​
 # Check if the request was successful (status code 200)
 if response.status_code == 200:
     # Parse the response JSON
     response_json = response.json()
     # Print the 'message' from the response
+    print(f"第{i + 1}个账号")
     print("Message:", response_json.get('message'))
+    #print(response_json.get('business'))
 else:
     print("Request failed with status code:", response.status_code)
